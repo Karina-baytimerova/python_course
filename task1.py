c0d7d20739d9eb2545ef1ee0552252127823873b
Karina-baytimerova <113613906+Karina-baytimerova@users.noreply.github.com>
2024-11-25 01:29:15 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -10,6 +10,5 @@ number_of_months = 0
 while money_capital >= 0:
     number_of_months += 1
     money_capital = money_capital + salary - spend
-    spend *= (1+increase)
-print("Количество месяцев, которое можно протянуть без долгов:", number_of_months-1)
-#
+    spend *= (1 + increase)
+print("Количество месяцев, которое можно протянуть без долгов:", number_of_months - 1, end='\n')
