d8ef3ae9ea76f74f879328eacf228711366629a3
Karina-baytimerova <113613906+Karina-baytimerova@users.noreply.github.com>
2024-11-28 00:37:27 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -8,3 +8,4 @@ disk_bytes = disk_mb * 1024 * 1024
 book_bytes = pages * lines_in_page * chars_in_line * bytes_in_char
 books_on_disk = int(disk_bytes // book_bytes)
 print("Количество книг, помещающихся на дискету:", books_on_disk, end='\n')
+ 
